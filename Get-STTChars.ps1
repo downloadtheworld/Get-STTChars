a937202e1a2a7acd8f3ae759c86f6c0509ec837e
knelmes <knelmes@gmail.com>
2018-02-20 13:59:35 +0000

    Major update to use api

--- Get-STTChars.ps1
+++ Get-STTChars.ps1
@@ -1,53 +1,41 @@
-﻿$rarities = "Common","Uncommon","Rare","Super_Rare","Legendary"
+#For when there is more than 500 in a category:
+
+#Raw information about the members of a category, their sortkeys and timestamps (time when last added to the category) can be obtained from the API, using a query of the form:
+
+#http://en.wikipedia.org/w/api.php?cmtitle=Category:Category_name&action=query&list=categorymembers&cmlimit=500&cmprop=title|sortkey|timestamp
+#Listings of up to 500 members are possible. If there are more members then the results will include text near the end like this: <categorymembers cmcontinue="page|NNNN|TITLE" />.
+
+#This can be added to the previous one, without quotation marks, for the next page of members: ...&cmcontinue=page|NNNN|TITLE
+
+
+
+
+
+
+
+
+$rarities = "Common","Uncommon","Rare","Super_Rare","Legendary"
 #$rarities = "Uncommon"
 $file = '.\STTChars.xlsx'
 $newchars = @()
 foreach ($rarity in $rarities){
-    $characters = Invoke-WebRequest https://stt.wiki/wiki/Category:$rarity -UseBasicParsing
-    #$characters.links.href
+    $request = 'https://stt.wiki/w/api.php?cmtitle=Category:' + $rarity + '&action=query&list=categorymembers&cmlimit=500&cmprop=title|sortkey|timestamp&format=json'
+$request
+    $json = Invoke-WebRequest $request
+    $json
+    $converted = ConvertFrom-Json -InputObject $json
+    $converted.query.categorymembers
+    Write-Host "converted"
 
-    $begin = ""
-    $end = ""
-    if($rarity -eq "Common"){
-        $begin = "/wiki/Cadets"
-        $end = "https://stt.wiki/w/index.php?title=Category:Common&amp;oldid=104908"
-    }
-    if($rarity -eq "Uncommon"){
-        $begin = "/wiki/Cadets"
-        $end = "https://stt.wiki/w/index.php?title=Category:Uncommon&amp;oldid=105597"
-    }
-    if($rarity -eq "Rare"){
-        $begin = "#p-search"
-        $end = "https://stt.wiki/w/index.php?title=Category:Rare&amp;oldid=105227"
-    }
-    if($rarity -eq "Super_Rare"){
-        $begin = "#p-search"
-        $end = "https://stt.wiki/w/index.php?title=Category:Super_Rare&amp;oldid=105562"
-    }
-    if($rarity -eq "Legendary"){
-        $begin = "#p-search"
-        $end = "https://stt.wiki/w/index.php?title=Category:Legendary&amp;oldid=105122"
-    }
-
-    $begun = 0
     $chars = @()
-    foreach ($link in $characters.Links){
-        if ($link.href -eq $end) {
-            break
-        }
-        if ($begun -eq "1"){
-            $char = New-Object –TypeName PSObject
-            $char | Add-Member –MemberType NoteProperty -Name "Name" -Value $link.title
-            $char | Add-Member –MemberType NoteProperty -Name "Link" -Value $link.href
-            $char | Add-Member –MemberType NoteProperty -Name "Rarity" -Value $rarity
-            $chars += $char
-        
-        }
-        if ($link.href -eq $begin) {
-            $begun = 1
-        }
+    foreach ($link in $converted.query.categorymembers){
+
+        $char = New-Object –TypeName PSObject
+        $char | Add-Member –MemberType NoteProperty -Name "Name" -Value $link.title
+        $char | Add-Member –MemberType NoteProperty -Name "Rarity" -Value $rarity
+        $chars += $char
     }
-    $chars.Count
+    #$chars.Count
     $newchars += $chars
 }
 if (Test-Path $file){
@@ -64,5 +52,5 @@ if (Test-Path $file){
     
     }
 }
-$newchars | Export-Excel $file
+$newchars | Export-Excel $file -BoldTopRow -AutoFilter -FreezeTopRow
 $newchars.Count
